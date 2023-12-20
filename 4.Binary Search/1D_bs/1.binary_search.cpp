ee6e63bd140f3a62b703d92fc5b9869f0a0defac
Goutam-04 <goutamnayak+github1@gmail.com>
2023-12-20 20:36:46 +0530

    20.12.2023 p1

---
+++ 4.Binary Search/1D_bs/1.binary_search.cpp
@@ -0,0 +1,42 @@
+Problem statement: You are given a sorted array of integers and a target, your task is to search for the target in the given array.
+ Assume the given array does not contain any duplicate numbers.
+
+Let’s say the given array is = {3, 4, 6, 7, 9, 12, 16, 17} and target = 6.
+
+//iterative solution
+
+int binarySearch(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+So the overall time complexity is O(logN), where N = size of the given array.
+
+//recursive solution
+
+int binarySearch(vector<int>& nums, int low, int high, int target) {
+
+    if (low > high) return -1; //Base case.
+
+    // Perform the steps:
+    int mid = (low + high) / 2;
+    if (nums[mid] == target) return mid;
+    else if (target > nums[mid])
+        return binarySearch(nums, mid + 1, high, target);
+    return binarySearch(nums, low, mid - 1, target);
+}
+
+int search(vector<int>& nums, int target) {
+    return binarySearch(nums, 0, nums.size() - 1, target);
+}
+
+So the overall time complexity is O(logN), where N = size of the given array.
