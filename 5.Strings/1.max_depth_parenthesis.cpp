33b2c266ce0f8d5796a2b83c73627afac3f7ccf9
Goutam-04 <goutamnayak+github1@gmail.com>
2023-12-26 22:24:52 +0530

    26-12-23 max-depth of parenthesisi

---
+++ 5.Strings/1.max_depth_parenthesis.cpp
@@ -0,0 +1,67 @@
+// You are given a string 'S' of length 'N' representing an integer.
+
+
+
+// A string is a valid parenthesis if it satisfies one of the following conditions:
+
+// If the string is empty or consists of a character other than ‘(‘ and ')'.
+
+// If the string can be represented as a concatenation of two strings such that both strings are valid parentheses strings.
+
+// If the string can be represented as '(A)', where A is a valid parenthesis.
+
+
+// Depth of a string is defined as follows:
+
+// depth( '' )=0
+
+// depth( A )=0, where ‘A’ is an empty string or consists of a character other than ‘(‘ and ‘)’.
+
+// depth( A+B )=max(depth( A ), depth( B )), such that ‘A’ and ‘B’ are both strings are valid parentheses strings.
+
+// depth( (A) ) = 1+depth(A), where ‘A’ is a valid parentheses string.
+
+
+// Given a valid parentheses string ‘S’, you must return the nesting depth of the string ‘S’.
+
+
+
+// Example:
+
+// Input:
+// S = '1+(3*6+(9-3))', N=13
+// Output: 2
+
+// Explanation: The digits 9 and 3 are inside of 2 nested parentheses. Hence we return 2.
+// Detailed explanation ( Input/output format, Notes, Images )
+// Sample Input 1:
+// 15
+// (3*(4*(5*(6))))
+// Sample Output 1:
+// 4
+// Explanation Of Sample Input 1:
+// Input:
+// S = '(3*(4*(5*(6))))', N = 15
+// Output: 4
+
+// Explanation: The digit 6 is inside of 4 nested parentheses. Hence we return 4.
+// Sample Input 2:
+// 11
+// (((((5)))))    
+// Sample Output 2:
+// 5
+
+int maxDepth(string s) {
+	// Write your code here.
+	int n=s.size();
+	int ans=0;
+	for(int i=0;i<n;i++){
+		if(s[i]=='(')ans--;
+		if(s[i]==')')ans++;
+		if(ans<0)ans=0;
+	}
+	return ans;
+}
+// EDge case
+
+// ((0-9))(1-3)(((4+5)((0/2)(5-1)(5/9))(9-0)((4/3)(2+7))(3-6)(((6+2)))))
